e84b837371cc53daf8fc0532bf040e7cf290c165
Elena Nedorezova <nedorezova.emp@gmail.com>
2023-03-13 16:43:47 +0300

    add BooksRestApiControllerArgCatcherAspect

---
+++ src/main/java/com/example/MyBookShopApp/aspects/BooksRestApiControllerArgCatcherAspect.java
@@ -0,0 +1,26 @@
+package com.example.MyBookShopApp.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class BooksRestApiControllerArgCatcherAspect {
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    @Pointcut(value = "execution(* books*()) && within(com.example.MyBookShopApp.controllers.BooksRestApiController))")
+    public void booksRestApiControllerArgCatcherPointcut(){}
+
+    @AfterReturning(pointcut = "booksRestApiControllerArgCatcherPointcut()", returning = "response")
+    public void booksRestApiControllerArgCatcherAdvice(JoinPoint joinPoint, ResponseEntity response){
+            logger.info(joinPoint.getTarget().getClass().getSimpleName() + "was invoked" +
+                        "returning ResponseEntity" + response.toString());
+    }
+}
